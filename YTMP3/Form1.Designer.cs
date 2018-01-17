41bb4dbd77a1da64378e136759e95e2b75626a1e
Przemysław Ida <p.ida@tuyo.pl>
2018-01-17 19:37:31 +0100

    Dodaj pliki projektów.

---
+++ YTMP3/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace YTMP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "https://www.youtube.com/playlist?list=PLaVgtnUO3oThsS203Amq4MQK3tgUshz3i";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Link do playlisty:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(423, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Dałnlołd";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 87);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Program w sumie do ściagania mp3 z youtube";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ YTMP3/Form1.cs
@@ -0,0 +1,88 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace YTMP3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool WalidujLink()
+        {
+            string link = textBox1.Text;
+            if (link == null)
+            {
+                MessageBox.Show("Wpisz coś tam.");
+                return false;
+            }
+            if (link.Length < 10)
+            {
+                MessageBox.Show("Coś za mało.");
+                return false;
+            }
+            if (!link.Contains("youtube") && !link.Contains("yt"))
+            {
+                MessageBox.Show("To nie jest link do youtube.");
+                return false;
+            }
+            return true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (WalidujLink())
+            {
+                PobierzAsync();
+            }
+        }
+
+        public async Task PobierzAsync()
+        {
+            try
+            {
+                var client = new YoutubeClient();
+                var url = textBox1.Text;
+                var id = YoutubeClient.ParsePlaylistId(url);
+
+                var playlist = await client.GetPlaylistAsync(id);
+
+                foreach (var vid in playlist.Videos)
+                {
+
+                    var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(vid.Id);
+                    var streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+                    var ext = streamInfo.Container.GetFileExtension();
+                    var video = await client.GetVideoAsync(vid.Id);
+                    string sourcePath = $"C:/YTMP3/{video.Title}.{ext}";
+                    string outputPath = $"C:/YTMP3/{video.Title}.mp3";
+                    await client.DownloadMediaStreamAsync(streamInfo, sourcePath);
+
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(sourcePath, outputPath, Format.mp4);
+
+                    File.Delete(sourcePath);
+                }
+                MessageBox.Show("Pobrałem.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Coś poszło nie tak." + Environment.NewLine + ex.Message);
+            }
+        }
+    }
+}
